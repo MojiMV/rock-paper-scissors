3e20502390308727b1a6e4baf48b1c9002689f5b
Moji MV <mojtabavahabnezhad@gmail.com>
2023-12-26 12:18:37 +0330

    fix the bugs and logic of the game and make it play properly by changing functions in javascript.js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+
 function getUserChoice(){
     choice = prompt("Make your choice (rock, paper, scissors)");
     choice = choice.toLowerCase()
@@ -10,28 +11,31 @@ function getUserChoice(){
         return choice;
     } else {
         alert("Wrong choice");
-    }
-
+    }  
 }
+
 function playOneRound() {
 
-    computerSelection = getComputerChoice();
     playerSelection = getUserChoice();
+    console.log(playerSelection);
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+
 
     if (computerSelection === "rock" && playerSelection === "rock"){
-        playOneRound();
+        return playOneRound();
     } else if (computerSelection === "rock" && playerSelection === "paper"){
         return true;
     } else if (computerSelection === "rock" && playerSelection === "scissors"){
         return false;
     } else if (computerSelection === "paper" && playerSelection === "paper"){
-        playOneRound();
+        return playOneRound();
     } else if (computerSelection === "paper" && playerSelection === "rock"){
         return false;
     } else if (computerSelection === "paper" && playerSelection === "scissors"){
         return true;
     } else if (computerSelection === "scissors" && playerSelection === "scissors"){
-        playOneRound();
+        return playOneRound();
     } else if (computerSelection === "scissors" && playerSelection === "rock"){
         return true;
     } else if (computerSelection === "scissors" && playerSelection === "paper"){
@@ -41,23 +45,31 @@ function playOneRound() {
 
 function game(){
     let wins = 0;
+    console.log(wins);
     let losses = 0;
+    console.log(losses);
 
     for (let i = 0; i<5 ; i++){
         x = playOneRound();
-        if (x === true){
+        if (x){
             wins = wins + 1;
             alert("You won this round!");
-        } else if (x === false){
+            
+        } else {
             losses = losses + 1;
             alert("You lost this round!");
+            
         }
+        console.log(wins);
+        console.log(losses);
     }
 
     if (wins > losses){
         alert("You won the game!");
     } else if( losses > wins) {
         alert("You lost the game!");
+    } else{
+        alert("It was a draw!")
     }
 }
 
